1d1756c9177cb1b8509436cbb9920d547327fe92
Pioneer220 <morrishart0220@gmail.com>
2025-01-10 14:21:37 +0000

    fix:social_share function

--- lib/screen/logIn/logIn2.dart
+++ lib/screen/logIn/logIn2.dart
@@ -5,17 +5,17 @@ import 'package:airline_app/utils/app_routes.dart';
 import 'package:airline_app/utils/app_styles.dart';
 import 'package:airline_app/utils/global_variable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:otpless_flutter/otpless_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:airline_app/screen/app_widgets/loading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:airline_app/controller/get_airline_controller.dart';
 import 'package:airline_app/controller/get_reviews_airline_controller.dart';
+
 import 'package:airline_app/provider/airline_airport_review_provider.dart';
 import 'package:airline_app/provider/airline_airport_data_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'loading_screen.dart'; // Import the loading screen
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Login extends ConsumerStatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -53,7 +53,7 @@ class _LoginState extends ConsumerState<Login> {
       await _fetchDataAndNavigate();
     } else {
       // No token, initialize Otpless
-      await testLogin();
+      await _initializeOtpless();
     }
     setState(() {
       isLoading = false;
@@ -78,52 +78,105 @@ class _LoginState extends ConsumerState<Login> {
     Navigator.pushReplacementNamed(context, AppRoutes.leaderboardscreen);
   }
 
-  Future<void> testLogin() async {
+  Future<void> _initializeOtpless() async {
+    await _otplessFlutterPlugin.enableDebugLogging(true);
+    await _otplessFlutterPlugin.initHeadless(appId);
+    _otplessFlutterPlugin.setHeadlessCallback(onHeadlessResult);
+  }
+
+  void onHeadlessResult(dynamic result) async {
+    String jsonString = jsonEncode(result);
     final response;
-    response = await http.post(
-      Uri.parse('$apiUrl/api/v1/user'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: json.encode({
-        'name': 'Morris Hart',
-        'whatsappNumber': '',
-        'email': 'morrishart0220@gmail.com',
-      }),
-    );
 
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      ref.read(userDataProvider.notifier).setUserData(responseData);
+    if (result != null && result['data'] != null) {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return Container(
+            color: Colors.black.withOpacity(0.5),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: const Center(
+                child: LoadingWidget(),
+              ),
+            ),
+          );
+        },
+      );
 
-      // Save token and userData to SharedPreferences
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('token', 'morrishart0220@gmail.com');
-      await prefs.setString('userData', json.encode(responseData));
+      UserData userData = UserData.fromJson(jsonString);
 
-      final airlineController = GetAirlineAirportController();
-      final result = await airlineController.getAirlineAirport();
-      if (result['success']) {
-        ref.read(airlineAirportProvider.notifier).setData(result['data']);
-      }
-
-      final reviewsController = GetReviewsAirlineController();
-      final reviewsResult = await reviewsController.getReviews();
-      if (reviewsResult['success']) {
-        ref
-            .read(reviewsAirlineProvider.notifier)
-            .setReviewData(reviewsResult['data']);
-      }
-
-      Navigator.pop(context); // Remove loading dialog
-
-      if (responseData['userState'] == 0) {
-        Navigator.pushReplacementNamed(context, AppRoutes.skipscreen);
+      if (userData.channel == 'WHATSAPP') {
+        response = await http.post(
+          Uri.parse('$apiUrl/api/v1/user'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: json.encode({
+            'name': userData.name,
+            'whatsappNumber': userData.identityValue,
+            'email': "",
+          }),
+        );
       } else {
-        Navigator.pushReplacementNamed(context, AppRoutes.leaderboardscreen);
+        response = await http.post(
+          Uri.parse('$apiUrl/api/v1/user'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: json.encode({
+            'name': userData.name,
+            'whatsappNumber': '',
+            'email': userData.identityValue,
+          }),
+        );
+      }
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        ref.read(userDataProvider.notifier).setUserData(responseData);
+
+        // Save token and userData to SharedPreferences
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('token', userData.idToken);
+        await prefs.setString('userData', json.encode(responseData));
+
+        final airlineController = GetAirlineAirportController();
+        final result = await airlineController.getAirlineAirport();
+        if (result['success']) {
+          ref.read(airlineAirportProvider.notifier).setData(result['data']);
+        }
+
+        final reviewsController = GetReviewsAirlineController();
+        final reviewsResult = await reviewsController.getReviews();
+        if (reviewsResult['success']) {
+          ref
+              .read(reviewsAirlineProvider.notifier)
+              .setReviewData(reviewsResult['data']);
+        }
+
+        Navigator.pop(context); // Remove loading dialog
+
+        if (responseData['userState'] == 0) {
+          Navigator.pushReplacementNamed(context, AppRoutes.skipscreen);
+        } else {
+          Navigator.pushReplacementNamed(context, AppRoutes.leaderboardscreen);
+        }
+      } else {
+        Navigator.pop(context); // Remove loading dialog
       }
     } else {
-      Navigator.pop(context); // Remove loading dialog
+      _showErrorSnackBar('Login failed. Please try again.');
+    }
+  }
+
+  Future<void> _loginWithWhatsApp() async {
+    try {
+      Map<String, dynamic> arg = {'appId': appId};
+      await _otplessFlutterPlugin.openLoginPage(onHeadlessResult, arg);
+    } catch (e) {
+      _showErrorSnackBar('WhatsApp login failed. Please try again.');
     }
   }
 
@@ -133,13 +186,13 @@ class _LoginState extends ConsumerState<Login> {
     return Scaffold(
       backgroundColor: AppStyles.mainColor,
       body: isLoading
-          ? const LoadingScreen() // Use the dedicated loading screen
+          ? const LoadingWidget()
           : Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SvgPicture.asset(
-                    'assets/images/login.svg', // Use your SVG image here
+                    'assets/images/login.svg',
                     width: screenSize.width,
                     height: screenSize.height * 0.74,
                     fit: BoxFit.cover,
@@ -147,7 +200,7 @@ class _LoginState extends ConsumerState<Login> {
                   Spacer(),
                   GestureDetector(
                     onTap: () {
-                      testLogin();
+                      _loginWithWhatsApp();
                     },
                     child: Padding(
                       padding:
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import geolocator_apple
 import google_sign_in_ios
 import mobile_scanner
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
 import video_player_avfoundation
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
