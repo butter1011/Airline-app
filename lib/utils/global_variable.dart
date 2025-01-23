61e5fdfe04ee1561964c534677d53b151d53ee82
butter1011 <luckybutter322982@gmail.com>
2025-01-23 14:19:47 +0900

    fix: fix the error

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,5 +1,5 @@
-// const apiUrl = "https://airlinereview-b835007a0bbc.herokuapp.com";
-// const backendUrl = "airlinereview-b835007a0bbc.herokuapp.com";
+const apiUrl = "https://airlinereview-b835007a0bbc.herokuapp.com";
+const backendUrl = "airlinereview-b835007a0bbc.herokuapp.com";
 const String appId = "bzpl0offchgcjrm8a4sj";
 const chatbotUrl = "https://airline-chatbot-ae62e84c30ae.herokuapp.com";
 const String ciriumUrl = "https://api.flightstats.com/flex/flightstatus/historical/rest/v3";
