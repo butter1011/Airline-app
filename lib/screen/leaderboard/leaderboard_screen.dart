c68155e60fe13a6594b0bc7d03828ce8175dad2e
Leopard <kanjirohonda@gmail.com>
2025-01-30 04:45:02 +0900

    refactor: update user ID retrieval in review submission screens and adjust button overlay color

--- lib/screen/leaderboard/leaderboard_screen.dart
+++ lib/screen/leaderboard/leaderboard_screen.dart
@@ -109,6 +109,7 @@ class _LeaderboardScreenState extends ConsumerState<LeaderboardScreen> {
       if (!mounted) return;
 
       if (futures[0]['success']) {
+        
         ref
             .read(reviewsAirlineProvider.notifier)
             .setReviewData(futures[0]['data']);
@@ -178,7 +179,7 @@ class _LeaderboardScreenState extends ConsumerState<LeaderboardScreen> {
   Future<void> _initializeData() async {
     await Future.wait([
       connectWebSocket(),
-      // fetchLeaderboardData(),
+      fetchLeaderboardData(),
     ]);
     setState(() {
       isLeaderboardLoading = false;
